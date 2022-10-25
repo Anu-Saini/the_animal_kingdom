31451e4ffdfedf32fb0915620aed92349a489c62
Anu.Saini <105421965+Anu-Saini@users.noreply.github.com>
2022-10-26 00:38:42 +1000

    partial components and pages

---
+++ animal_kingdom/client/src/components/CarouselImage.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Carousel.css";
+
+function CarouselImages(props) {
+  console.log(props);
+
+  // const src = `../assets/elephant.jpg`;
+  return (
+    <>
+      <div className="container">
+        <div className="imgbox">
+        <img src={props.props.image[0]}  alt={props.props.animalName} style={{width:'700px', height:'350px'}} />
+        </div>
+        <div className="aboutbox">
+        <div className="centered">{props.props.animalName} </div>
+
+        <div className="description">{props.props.description}</div>    
+        </div>
+              </div>
+    </>
+  );
+}
+
+export default CarouselImages;
